d98674a2105d22d148d48cd934a4c7857f7d46c6
Max Cramer <maximiliancramer@yahoo.com>
2020-04-29 13:56:21 +0100

    attempting to get nav working

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Menu from './components/menu/menu';
+import ProRiders from './components/proRiders/proRiders';
 
-function App() {
+
+function App(pageName) {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <Menu />
       </header>
+      <body>
+      <ProRiders />
+      </body>
     </div>
   );
 }
 
 export default App;
+
+
+
+
+// <img src={logo} className="App-logo" alt="logo" />
+//   <p>
+//     Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//   <a
+//     className="App-link"
+//     href="https://reactjs.org"
+//     target="_blank"
+//     rel="noopener noreferrer"
+//   >
+//     Learn React
+//         </a>
---
+++ src/test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+// import ReactDOM from 'react-dom';
+import './index.css';
+// import App from './App';
+
+function Test() {
+    return (
+        <h1>TESTING</h1>
+    )
+}
+
+export default Test;
