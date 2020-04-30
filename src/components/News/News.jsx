7c674a8c6176dd7a37057cad445b4fadb8cec853
Max Cramer <maximiliancramer@yahoo.com>
2020-04-30 15:00:13 +0100

    first object in both riders array and articles array displaying in view, to work out how to get others with to display at the same time with for loop

--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import { articles } from '../../Data/Articles';
+
 
 function News() {
+  for (var i = 0; i < articles.length; i++) {
+    var obj = articles[i];
+    console.table('Author Name is:', obj.Author);
+  }
+  // console.log('articles author is:', Articles.Author);
   return (
     <div>
       <h1>News</h1>
+      <h2>{obj.Title}</h2>
+      <h3>{obj.Author}</h3>
+      <p>{obj.Content}</p>
     </div>
   );
 }
--- src/components/proRiders/ProRiders.jsx
+++ src/components/proRiders/ProRiders.jsx
@@ -1,15 +1,41 @@
 import React from 'react';
 import { riders } from "../../Data/Riders";
 
-console.log(riders);
+// console.log(riders);
 
-function ProRiders(Riders) {
-  console.log(Riders.name);
-  return (
-    <div>
-      <h1>Pro Riders</h1>
-    </div>
-  );
+function ProRiders() {
+  for (var i = 0; i < riders.length; i++) {
+    var obj = riders[i];
+    console.log("The pro riders are:", obj.name);
+    return (
+      <div>
+        <h1>Pro Riders</h1>
+        <h3>{obj.name}</h3>
+      </div>
+    );
+  }
 }
 
 export default ProRiders;
+
+
+
+// import React from 'react';
+// import { articles } from '../../Data/Articles';
+
+// // var data = require('../../Data/Articles');
+
+// function News() {
+//   for (var i = 0; i < articles.length; i++) {
+//     var obj = articles[i];
+//     console.log('Name: ' + obj.Author);
+//   }
+//   // console.log('articles author is:', Articles.Author);
+//   return (
+//     <div>
+//       <h1>News</h1>
+//     </div>
+//   );
+// }
+
+// export default News;
