e5b9bb0dd9090fef49f1b4416d7b7756809e0dc0
Max Cramer <maximiliancramer@yahoo.com>
2020-04-30 10:56:46 +0100

    adding logo image to nav bar

--- src/components/Navigation/Nav.jsx
+++ src/components/Navigation/Nav.jsx
@@ -1,20 +1,25 @@
 import React from 'react';
 import '../../App.css';
 import {Link} from 'react-router-dom';
+import logo from '../../Images/OWN_logo.png';
+
+
+// import OWN_logo from '../../Images/OWN_logo';
+
 
 function Nav() {
   return (
     <nav>
-      <h2>Logo</h2>
+      <img id="nav_logo" src={logo} alt="logo" />
       <ul className="nav-links">
         <Link to="/">
           <li>Home</li>
         </Link>
-        <Link to="/proriders">
-          <li>Pro Riders</li>
-        </Link>
         <Link to="/news">
           <li>News</li>
+        </Link>
+        <Link to="/proriders">
+          <li>Pro Riders</li>
         </Link>
         <Link to="/boardcare">
           <li>Board Care</li>
