66373e9d586e9260b9778898be9507583454db73
Max Cramer <maximiliancramer@yahoo.com>
2020-04-30 13:40:46 +0100

    pro rider data passing from .json file though component

--- src/components/proRiders/ProRiders.jsx
+++ src/components/proRiders/ProRiders.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { riders } from "../../Data/Riders";
 
-function ProRiders() {
+console.log(riders);
+
+function ProRiders(Riders) {
+  console.log(Riders.name);
   return (
     <div>
-        <h1>Pro Riders</h1>
+      <h1>Pro Riders</h1>
     </div>
   );
 }
