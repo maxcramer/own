9523623bab23a45ea76fc84a0ee8c1a9d56145b1
Max Cramer <maximiliancramer@yahoo.com>
2020-05-07 13:12:19 +0100

    taking to new link on title click

--- src/components/News/FullPostItem.jsx
+++ src/components/News/FullPostItem.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import { Articles } from '../../Data/Articles';
 // link to - 
 const PostsItem = (props) => {
-  console.log(props)
-  const { Title, Content, Author } = props.article
-  console.log("this console log", props.article) 
-  
+  // console.log(props)
+  const { Title, Content, Author } = props.article;
+  console.log("this console log", props.article);
   return (
               <div>
+                <ul>
+                  {
+                    Articles.all().map(a => (
+                      <li key={a.id}>
+                        <Link to={`/news/${a.id}`}>{a.Title}</Link>
+                      </li>
+                    ))
+                  }
+                </ul>
                 <h3>{Title}</h3>
                 <p>{Content}</p>
                 <p>{Author}</p>
