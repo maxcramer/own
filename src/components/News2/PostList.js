ec2e6a51ed1b07ebbf3dee539954804a722fc8ab
Max Cramer <maximiliancramer@yahoo.com>
2020-05-11 15:18:20 +0100

    rerouting to use list components rather than going straight into mapping functionality for data

--- src/components/News2/PostList.js
+++ src/components/News2/PostList.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
+import News from './News2';
 import FullPostList from './FullPostList';
 import Post from './Post';
 
 const PostList = () => (
     <Switch>
-      <Route exact path='/postlist' component={FullPostList}/>
+      <Route exact path='/postlist' component={News}/>
       <Route path='/postlist/:id' component={Post}/>
     </Switch>
 )
--- src/components/proRiders/ProList.js
+++ src/components/proRiders/ProList.js
@@ -3,10 +3,11 @@ import { Switch, Route } from 'react-router-dom';
 
 import FullRiderList from './FullRiderList';
 import Rider from './Rider';
+import ProRidersPage from './ProRiderPage';
 
 const ProList = () => (
   <Switch>
-    <Route exact path='/prolist' component={FullRiderList}/>
+    <Route exact path='/prolist' component={ProRidersPage}/>
     <Route path='/prolist/:id' component={Rider}/>
   </Switch>
 )
