49b049ee949dfb2e666f469dd92cdedaa1d00b26
Max Cramer <maximiliancramer@yahoo.com>
2020-05-11 10:49:09 +0100

    correcting rider data structure

--- src/Data/Articles.js
+++ src/Data/Articles.js
@@ -1,30 +1,30 @@
 export const Articles = {
    articles: [ 
      {
-      "id": "1",
-      "Author": "Frank",
-      "dateUpload": "02.22.20",
-      "Title": "IpsumLorem",
-      "Content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+      id: 1,
+      Author: "Frank",
+      dateUpload: "02.22.20",
+      Title: "IpsumLorem",
+      Content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     },
     {
-      "id": "2",
-      "Author": "Pete",
-      "dateUpload": "02.22.20",
-      "Title": "IpsumLorem",
-      "Content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+      id: 2,
+      Author: "Pete",
+      dateUpload: "02.22.20",
+      Title: "IpsumLorem",
+      Content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     },
     {
-      "id": "3",
-      "Author": "Dave",
-      "dateUpload": "02.22.20",
-      "Title": "IpsumLorem",
-      "Content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+      id: 3,
+      Author: "Dave",
+      dateUpload: "02.22.20",
+      Title: "IpsumLorem",
+      Content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     }
   ],
   all: function() { return this.articles },
   get: function(id) {
-    const isArticle = a => a.id == id
+    const isArticle = a => a.id === id
     return this.articles.find(isArticle)
   }
 };
--- src/Data/Riders.js
+++ src/Data/Riders.js
@@ -1,31 +1,31 @@
 export const Riders ={
-  Riders: [
+  riders: [
   {
-    "id": "1",
-    "name": "Chris Richardson",
-    "location": "Dummy Location",
-    "Competitions": "",
-    "Sponsonrs": "",
-    "Ig": "",
-    "Fb": ""
+    id: 1,
+    name: "Chris Richardson",
+    location: "Dummy Location",
+    Competitions: "",
+    Sponsonrs: "",
+    Ig: "",
+    Fb: ""
   },
   {
-    "id": "2",
-    "name": "Max Cramer",
-    "location": "London, UK",
-    "Competitions": "Non",
-    "Sponsonrs": "Wick Boards | HopBar UK | SkatetecUK | OWN",
-    "Ig": "http://instagram.com/m__row",
-    "Fb": ""
+    id: 2,
+    name: "Max Cramer",
+    location: "London, UK",
+    Competitions: "Non",
+    Sponsonrs: "Wick Boards | HopBar UK | SkatetecUK | OWN",
+    Ig: "http://instagram.com/m__row",
+    Fb: ""
   },
   {
-    "id": "3",
-    "name": "Jeff McCosker",
-    "location": "Sacramento, CA, USA",
-    "Competitions": "Non",
-    "Sponsonrs": "The Float Life (Owner)",
-    "Ig": "http://instagram.com/thefloat.life",
-    "Fb": ""
+    id: 3,
+    name: "Jeff McCosker",
+    location: "Sacramento, CA, USA",
+    Competitions: "Non",
+    Sponsonrs: "The Float Life (Owner)",
+    Ig: "http://instagram.com/thefloat.life",
+    Fb: ""
   }
 ]
 }
