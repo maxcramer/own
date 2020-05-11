23616c72e2962121e8f4165e93b921abe679e465
Max Cramer <maximiliancramer@yahoo.com>
2020-05-11 11:30:21 +0100

    maping over data to display list of rider names as links in list

--- src/components/proRiders/proRiderPage.js
+++ src/components/proRiders/proRiderPage.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ProRoutes from './ProRoutes';
+import FullRiderList from './FullRiderList';
 
 function ProRiders() {
     return (
         <div>
             <h1>Pro Rider Interviews</h1>
-            <ProRoutes />
+            <FullRiderList />
         </div>
     )
 }
