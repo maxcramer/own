41b5929f9967f9ae608bba310f7abea259b3928d
Max Cramer <maximiliancramer@yahoo.com>
2020-05-13 13:16:56 +0100

    cleaning up sortPost file and putting function into single line function

--- src/components/News2/posts/SortPost.jsx
+++ src/components/News2/posts/SortPost.jsx
@@ -1,20 +1,6 @@
 import React from 'react';
 import {Articles} from '../../../Data/Articles';
 
-// let SortedList =  Articles.articles.sort((a, b) =>
-//  new Date(...a.Date.split('-')
-//  .sort(Date).join().localeCompare(b.Date.split('-').join())));
-
-// export default SortedList;
-
-function sortedPosts() {
-    console.log("sortedPosts function envoked");
-    Articles.articles.sort((a, b) => b.date - a.date);
-
-} 
-
-// const dateArray = Articles.articles.date;
-
-// const sortDates = 
+function sortedPosts() {Articles.articles.sort((a, b) => b.date - a.date)} 
 
 export default sortedPosts;
