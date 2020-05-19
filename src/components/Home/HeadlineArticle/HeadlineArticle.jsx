89c418cbc6f58adc66c0fba69865ec850c3bb932
Max Cramer <maximiliancramer@yahoo.com>
2020-05-19 15:20:41 +0100

    setting up headline component for home page file, class component

---
+++ src/components/Home/HeadlineArticle/HeadlineArticle.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+import Articles from '../../../Data/Articles';
+
+class HeadlineArticle extends React.Component {
+    constructor() {
+        super();
+    }
+
+    render() {
+        return (
+            <div>
+                <h2>Headline</h2>
+            </div>
+        )
+    }
+};
+
+export default HeadlineArticle;
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import HeadlineArticle from './HeadlineArticle/HeadlineArticle';
 
 function Home() {
   return (
     <div>
       <h1>OneWheel News Home Page</h1>
+      <HeadlineArticle />
     </div>
   );
 }
