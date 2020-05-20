157187f5d07ed696ddfe277081d8d282bbab2184
Max Cramer <maximiliancramer@yahoo.com>
2020-05-20 15:59:29 +0100

    Most recent article rendering in home page for headline article

--- src/components/Home/HeadlineArticle/HeadlineArticle.jsx
+++ src/components/Home/HeadlineArticle/HeadlineArticle.jsx
@@ -2,28 +2,20 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 
 import {Articles} from '../../../Data/Articles';
-import sortedPostsHeadline from './HeadlineArticleSort';
-// import HeadlineFilter from './HeadlineFilter';
 
 class HeadlineArticle extends React.Component {
     constructor() {
         super();
-        sortedPostsHeadline();
-        // HeadlineFilter();
     }
-
     render() {
+        const recentArticle = Articles.articles.sort((a, b) => b.date - a.date)[0]
+        
         return (
             <div>
                 <h2>Headline</h2>
-                {
-                    Articles.all().map(a => (
-                        <div key={a.id}>
-                            <h3>{a.date.toLocaleDateString()}</h3>
-                            <h3>{a.Title}</h3>
-                        </div>
-                    ))
-                }
+                <h3>{recentArticle.Title}</h3>
+                <h3>{recentArticle.Author}</h3>
+                <p>{recentArticle.date.toLocaleString()}</p>
             </div>
         )
     }
--- src/components/Home/HeadlineArticle/HeadlineArticleSort.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Articles } from '../../../Data/Articles';
-
-function sortedPostsHeadline() { Articles.articles.map(a => (
-    Articles.articles.sort((a, b) => b.date - a.date) 
-))}
-
-export default sortedPostsHeadline;
