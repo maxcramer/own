577d0edbf5787faa3b6a582bb94219a9bce7012a
Max Cramer <maximiliancramer@yahoo.com>
2020-05-21 15:57:04 +0100

    adding margin to home page title

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import HeadlineArticle from './HeadlineArticle/HeadlineArticle';
 import SubArticles from './SubHeadlines/SubHeadlinesComponent';
 
+import './Home.css';
+
 function Home() {
   return (
     <div>
-      <h1>OneWheel News Home Page</h1>
+      <h1 className="homepage_title">OneWheel News</h1>
       <HeadlineArticle />
       <SubArticles />
     </div>
