f05830804c664ff718bc23abdc51ffb5c565e2c9
Max Cramer <maximiliancramer@yahoo.com>
2020-05-22 13:11:54 +0100

    continued styling for interview one in home page, all done except for social links and read more link

--- src/components/Home/LatestInvterviews/Interview1.jsx
+++ src/components/Home/LatestInvterviews/Interview1.jsx
@@ -12,11 +12,13 @@ class InterviewOne extends React.Component {
                 <img className="rider_image" src={recentInterview.Logo} alt=""/>
                 <div className="rider_info">
                     <div className="rider_name_location">
-                        <h3 className="rider_name">{recentInterview.name}</h3>
-                        <p>Location: {recentInterview.location}</p>   
-                        <p>Date Uploaded: {recentInterview.dateUpload.toLocaleString()}</p>
+                        <h2 className="rider_name">{recentInterview.name}</h2>
+                        <h6 class="upload_date">Date Uploaded: {recentInterview.dateUpload.toLocaleString()}</h6>
                     </div>
-                    <p className="sponsors">Sponsors: {recentInterview.Sponsonrs}</p>
+                    <div className="sponsor_location">
+                        <h4 className="sponsors">Sponsors: {recentInterview.Sponsonrs}</h4>
+                        <h4>Location: {recentInterview.location}</h4>   
+                    </div>
                     <p className="interview">{recentInterview.Interview}</p>  
                 </div>
                 {/* <div className="pro_socials">
