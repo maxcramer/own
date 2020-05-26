88609b79e9e7295fca47eba7baf3ccc64c7b6e67
Max Cramer <maximiliancramer@yahoo.com>
2020-05-26 11:59:01 +0100

    styled third pro rider interview, home page interview section styled in complete

--- src/Data/Riders.js
+++ src/Data/Riders.js
@@ -32,6 +32,7 @@ export const Riders = {
       Competitions: "Non",
       Sponsonrs: "The Float Life (Owner)",
       Interview: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      Logo: 'https://i.imgur.com/oulPj1C.jpg',
       Ig: "http://instagram.com/thefloat.life",
       Fb: ""
     }
--- src/components/Home/LatestInvterviews/Interview3.jsx
+++ src/components/Home/LatestInvterviews/Interview3.jsx
@@ -1,15 +1,32 @@
 import React from 'react';
 
 import { Riders } from '../../../Data/Riders';
+import IG from '../../../Images/Instagram-Icon.png'
+
 
 class InterviewThree extends React.Component {
     render() {
         const recentInterviewThree = Riders.riders.sort((a, b) => b.dateUpload - a.dateUpload)[2]
         return (
-            <div>
-                <h3>{recentInterviewThree.name}</h3>
-                <p>{recentInterviewThree.dateUpload.toLocaleString()}</p>
-                <p>{recentInterviewThree.location}</p>
+            <div className="pro_rider_interview">
+                <img className="rider_image" src={recentInterviewThree.Logo} alt="" />
+                <div className="rider_info">
+                    <div className="rider_name_location">
+                        <h2 className="rider_name">{recentInterviewThree.name}</h2>
+                        <h6 class="upload_date">Date Uploaded: {recentInterviewThree.dateUpload.toLocaleString()}</h6>
+                    </div>
+                    <div className="sponsor_location">
+                        <h4 className="sponsors">Sponsors: {recentInterviewThree.Sponsonrs}</h4>
+                        <h4>Location: {recentInterviewThree.location}</h4>
+                    </div>
+                    <p className="interview">{recentInterviewThree.Interview}</p>
+                    <div className="pro_socials">
+                        <a href={recentInterviewThree.Ig}>
+                            <img className="ig_link" src={IG} alt="" />
+                        </a>
+                        <p>{recentInterviewThree.Fb}</p>
+                    </div>
+                </div>
             </div>
         )
     }
