3e6dfcb7ed8ba98f2f27a67305eb0b48bb5c7b57
Max Cramer <maximiliancramer@yahoo.com>
2020-05-27 14:46:00 +0100

    adding upload date to single page interview title

--- src/components/proRiders/pros/Rider.js
+++ src/components/proRiders/pros/Rider.js
@@ -15,19 +15,23 @@ const Rider = (props) => {
     }
     return (
         <div className="interview_single_page_container">
-            <h2>{Rider.name}</h2>
+            <div className="interview_single_page_title_container">
+                <div className="title_without_date">
+                    <h2 className="interview_single_page_name">{Rider.name}</h2>
+                    <a className="interview_single_page_ig_link_tag" href={Rider.Ig}>
+                        <img className="interview_single_page_ig_link" src={IG} alt="" />
+                    </a>
+                </div> 
+                <p className="interview_single_page_upload_date">Uploaded on {Rider.dateUpload.toLocaleDateString()}</p>
+            </div>
             <div className="interview_single_page_rider_info">
-                <h3 className="interview_single_page_font_weight">Sponsorship: {Rider.Sponsonrs}</h3>            
-                <h3 className="interview_single_page_font_weight">Location: {Rider.location}</h3>
+            <h3 className="interview_single_page_font_weight">Sponsorship: {Rider.Sponsonrs}</h3>            
+            <h3 className="interview_single_page_font_weight">Location: {Rider.location}</h3>
             </div>
             <div className="interview_single_page_content">
                 <img className="interview_single_page_img" src={Rider.Logo} alt="" />
                 <p>{Rider.Interview}</p>            
             </div>
-            <p>{Rider.dateUpload.toLocaleDateString()}</p>
-            <a href={Rider.Ig}>
-                <img className="ig_link" src={IG} alt="" />
-            </a>
             <Link to={'/prolist'}>Go Back</Link>
         </div>
     )
