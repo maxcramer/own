895d66b8a0fd8365ce60a99ca7a2ae775c4233ed
Max Cramer <maximiliancramer@yahoo.com>
2020-05-27 14:16:45 +0100

    styled content, img - interview on single interview page

--- src/components/proRiders/pros/Rider.js
+++ src/components/proRiders/pros/Rider.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Link } from 'react-router-dom'
 
 import {Riders} from '../../../Data/Riders';
+import '../proRiders.css';
+import IG from '../../../Images/Instagram-Icon.png'
 
 const Rider = (props) => {
     console.log("Reached the rider page");
@@ -12,10 +14,20 @@ const Rider = (props) => {
         return <div>Sorry but no Rider was found</div>
     }
     return (
-        <div>
-            <h1>Pro rider single page </h1>
+        <div className="interview_single_page_container">
             <h2>{Rider.name}</h2>
-            <h3>{Rider.location}</h3>
+            <div className="interview_single_page_rider_info">
+                <h3 className="interview_single_page_font_weight">Sponsorship: {Rider.Sponsonrs}</h3>            
+                <h3 className="interview_single_page_font_weight">Location: {Rider.location}</h3>
+            </div>
+            <div className="interview_single_page_content">
+                <img className="interview_single_page_img" src={Rider.Logo} alt="" />
+                <p>{Rider.Interview}</p>            
+            </div>
+            <p>{Rider.dateUpload.toLocaleDateString()}</p>
+            <a href={Rider.Ig}>
+                <img className="ig_link" src={IG} alt="" />
+            </a>
             <Link to={'/prolist'}>Go Back</Link>
         </div>
     )
