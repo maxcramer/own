ac2696f82f35bf570ddd6d7003c2a1a5dc043773
Max Cramer <maximiliancramer@yahoo.com>
2020-05-28 13:40:17 +0100

    commit for Michael to help with firebase set up

--- src/components/LikeButton/LikeButton.jsx
+++ src/components/LikeButton/LikeButton.jsx
@@ -1,29 +1,59 @@
+import * as firebase from 'firebase/app'
 import React from 'react';
-import firebase from 'firebase';
+import ReactDOM from 'react-dom'
 
-import Like from '../../Images/Facebook-Like-PNG-HD.png';
-import './LikeButton.css';
 
 
 firebase.initializeApp({
-    databaseURL: 'https://onewheelnews.firebaseio.com'
+    databaseURL: 'https://react-firebase-sandbox.firebaseio.com'
 });
-const { connect } = reactFirebase;
 
-const Counter = ({ count, setCounter }) => (
-    <div>
-        <p>Count: { count || 0 }</p>
-        <button onClick={() => setCounter(count + 1)}>Increment</button>
-    </div>
-)
+const { connect } = firebase;
+
+const Counter = ({ count, setCount }) => (
+  <div>
+    <p>Count: {count || 0}</p>
+    <button onClick={() => setCount(count - 1)}>Decrement</button>
+    <button onClick={() => setCount(count + 1)}>Increment</button>
+  </div>
+);
 
 const ConnectedCounter = connect((props, ref) => ({
-    count: 'counterValue',
-    setCount: count => ref('counterValue').set(count),
-}))(Counter)
-    
+  count: 'counterValue',
+  setCount: count => ref('counterValue').set(count)
+}))(Counter);
 
 export default ConnectedCounter;
+
+ReactDOM.render(<ConnectedCounter />, document.getElementById('root'));
+
+// import React from 'react';
+// import * as firebase from 'firebase/app';
+// import { connect } from 'firebase'
+
+// import Like from '../../Images/Facebook-Like-PNG-HD.png';
+// import './LikeButton.css';
+
+
+// firebase.initializeApp({
+//     databaseURL: 'https://onewheelnews.firebaseio.com'
+// });
+// const { connect } = reactFirebase;
+
+// const Counter = ({ count, setCounter }) => (
+//     <div>
+//         <p>Count: { count || 0 }</p>
+//         <button onClick={() => setCounter(count + 1)}>Increment</button>
+//     </div>
+// )
+
+// const ConnectedCounter = connect((props, ref) => ({
+//     count: 'counterValue',
+//     setCount: count => ref('counterValue').set(count),
+// }))(Counter)
+    
+
+// export default ConnectedCounter;
     // class Counter extends React.Component {
     // state = {
     //     count: 0
