1d73524d7d8308859f064ade30932dff6bd26c1c
Max Cramer <maximiliancramer@yahoo.com>
2020-05-28 11:54:52 +0100

    trying to get like button working with firebase

--- src/components/LikeButton/LikeButton.jsx
+++ src/components/LikeButton/LikeButton.jsx
@@ -4,25 +4,45 @@ import firebase from 'firebase';
 import Like from '../../Images/Facebook-Like-PNG-HD.png';
 import './LikeButton.css';
 
-class Counter extends React.Component {
-    state = {
-        count: 0
-    }
 
-    handleClick = () => {
-        this.setState(prev => ({ count: prev.count + 1}));
-    }
+firebase.initializeApp({
+    databaseURL: 'https://onewheelnews.firebaseio.com'
+});
+const { connect } = reactFirebase;
 
-    render() {
-        return (
-            <div className="like_button_container">
-                <button className="like_button" onClick={this.handleClick}>
-                    <img className="like_button_img" src={Like} alt=""/>
-                </button>
-                <p>{this.state.count}</p>
-            </div>
-        );
-    }
-}
+const Counter = ({ count, setCounter }) => (
+    <div>
+        <p>Count: { count || 0 }</p>
+        <button onClick={() => setCounter(count + 1)}>Increment</button>
+    </div>
+)
 
-export default Counter;
+const ConnectedCounter = connect((props, ref) => ({
+    count: 'counterValue',
+    setCount: count => ref('counterValue').set(count),
+}))(Counter)
+    
+
+export default ConnectedCounter;
+    // class Counter extends React.Component {
+    // state = {
+    //     count: 0
+    // }
+
+    // handleClick = () => {
+    //     this.setState(prev => ({ count: prev.count + 1}));
+    // }
+
+    // render() {
+    //     return (
+    //         <div className="like_button_container">
+    //             <button className="like_button" onClick={this.handleClick}>
+    //                 <img className="like_button_img" src={Like} alt=""/>
+    //             </button>
+    //             <p>{this.state.count}</p>
+    //         </div>
+    //     );
+    // }
+// }
+
+// export default Counter;
--- src/components/News2/posts/Post.js
+++ src/components/News2/posts/Post.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {Link} from 'react-router-dom'
 
 import {Articles} from '../../../Data/Articles';
-import LikeButton from '../../LikeButton/LikeButton';
+import ConnectedCounter from '../../LikeButton/LikeButton';
 import './postStyle.css';
 
 const Post = (props) => {
@@ -14,21 +14,24 @@ const Post = (props) => {
         return <div>Sorry, but the Article was not found</div>;
     }
     return (
-        <div className="single_post_page">
-            <img className="single_post_img" src={Post.image} alt=""/>
-            <h1 className="single_post_title">{Post.Title}</h1>
-            <div className="single_post_author_upload">
-                <h3 className="single_post_author">By {Post.Author}</h3>
-                <h3 className="single_post_author">Uploaded {Post.date.toLocaleDateString()}</h3>  
-            </div>
-            <p className="single_post_content">{Post.Content}</p>
-            <div className="cta_single_post">
-                <Link className="read_more_tag" to={'/postlist'}>See All Articles</Link>
-                <LikeButton />
-            
-            </div>
+      <div className="single_post_page">
+        <img className="single_post_img" src={Post.image} alt="" />
+        <h1 className="single_post_title">{Post.Title}</h1>
+        <div className="single_post_author_upload">
+          <h3 className="single_post_author">By {Post.Author}</h3>
+          <h3 className="single_post_author">
+            Uploaded {Post.date.toLocaleDateString()}
+          </h3>
         </div>
-    )
+        <p className="single_post_content">{Post.Content}</p>
+        <div className="cta_single_post">
+          <Link className="read_more_tag" to={'/postlist'}>
+            See All Articles
+          </Link>
+          <ConnectedCounter />
+        </div>
+      </div>
+    );
 }
 
 export default Post;
