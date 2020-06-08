52be89e077b70d09d745e2491fa040316cdabf9e
Max Cramer <maximiliancramer@yahoo.com>
2020-06-08 17:56:57 +0100

    search component maping over all objects but not filtering, will work on tomorrow

---
+++ src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import {Articles} from '../../Data/Articles';
+
+function SearchBar() {
+    const [searchTerm, setSearchTerm] = React.useState('');
+    const [searchResults, setSearchResults] = React.useState([]);
+    const handleChange = event => {
+      setSearchTerm(event.target.value);
+    };
+
+    React.useEffect(() => {
+        const results = Articles.articles.filter(article => 
+            article.toString().toLowerCase().includes(searchTerm)
+            );
+            setSearchResults(results);
+    }, [searchTerm])
+
+    return (
+      <div>
+        <input
+          type="text"
+          placeholder="Search"
+          value={searchTerm}
+          onChange={handleChange}
+        />
+        <ul>
+            {searchResults.map(article => (
+                <li>{[article.Title]}</li>
+            ))}
+        </ul>
+      </div>
+    );
+} 
+
+export default SearchBar;
