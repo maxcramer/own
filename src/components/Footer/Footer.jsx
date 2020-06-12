7027d698c4a98a2a310ef36e6f701b1571f3a4cd
Max Cramer <maximiliancramer@yahoo.com>
2020-06-12 14:19:05 +0100

    adding styling for footer desktop

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
 
 import IG from '../../Images/Instagram-Icon.png';
 import './Footer.css';
@@ -12,19 +11,23 @@ const Footer = props => (
       </div>
       <div id="quick-links">
         <h4>Quick Links</h4>
-        <ul>
+        <ul id="quick-links-list">
           <li>link</li>
           <li>link</li>
         </ul>
       </div>
-    </div>
-    <div id="socials-copyright">
-      <p>Copyright Ⓒ All Rights Reserved By Onewheel News</p>
       <div id="socials">
+        <h4>Social Links</h4>
         <a href="https://www.instagram.com/onewheelnews/?hl=en">
           <img id="ig-logo" src={IG} alt="" />
         </a>
       </div>
+    </div>
+    <div id="socials-copyright">
+      <p>
+        Copyright Ⓒ 2020 All Rights Reserved By Onewheel News | No affiliation
+        with Future Motion
+      </p>
     </div>
   </div>
 );
