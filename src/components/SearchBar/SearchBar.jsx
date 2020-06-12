8ce30dc0e5823c295d3de85ae470dfdf7ad2a9af
Max Cramer <maximiliancramer@yahoo.com>
2020-06-12 12:36:19 +0100

    creating folder and files for Footer

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -3,17 +3,17 @@ import ReactDOM from 'react-dom';
 import { Link } from 'react-router-dom';
 
 import SearchImg from '../../Images/search-icon.png';
-import {Articles} from '../../Data/Articles';
-import {Riders} from '../../Data/Riders';
+import { Articles } from '../../Data/Articles';
+import { Riders } from '../../Data/Riders';
 
 import './SearchBar.css';
 
 function SearchBar() {
-    const [searchTerm, setSearchTerm] = React.useState('');
-    const [searchResults, setSearchResults] = React.useState([]);
-    const handleChange = event => {
-      setSearchTerm(event.target.value);
-    };
+  const [searchTerm, setSearchTerm] = React.useState('');
+  const [searchResults, setSearchResults] = React.useState([]);
+  const handleChange = event => {
+    setSearchTerm(event.target.value);
+  };
 
   // function isRider() {
   //   return (
@@ -21,82 +21,80 @@ function SearchBar() {
   //   )
   //   }
 
-    React.useEffect(() => {
-        const results1 = Articles.articles.filter(
-          article =>
-            article.Title.toString()
-              .toLowerCase()
-              .includes(searchTerm.toLocaleLowerCase()) 
-            //   ||
-            // article.name.toString()
-            //   .toLowerCase()
-            //   .includes(searchTerm.toLocaleLowerCase()) 
-            //   ||
-            // article.date
-            //   .toString()
-            //   .toLowerCase()
-            //   .includes(searchTerm.toLocaleLowerCase()) // THIS BREAKS IT ON SOME LETTERS SUCH AS A, C & G, WHY? Because its reading the string "Greenwich Mean Time", so technically all!
-            //   ||
-            // article.Content.toString()
-            //   .toLowerCase()
-            //   .includes(searchTerm.toLocaleLowerCase())// THIS WILL ALLOW THE SEARCH BAR TO SEARCH THROUGH ARTICLE CONTENT, TO MUCH?
-            );
-              
-        const results2 = Riders.riders.filter(
-            rider => 
-                rider.Title.toString()
-                .toLowerCase()
-                .includes(searchTerm.toLocaleLowerCase())
-                )
-                
-                const fullSearchResults =  results1.concat(results2);
-                fullSearchResults.sort((a, b) => (a.Title > b.Title) ? 1 : ((b.Title > a.Title) ? -1 : 0));
-                console.log(fullSearchResults);
+  React.useEffect(() => {
+    const results1 = Articles.articles.filter(
+      article =>
+        article.Title.toString()
+          .toLowerCase()
+          .includes(searchTerm.toLocaleLowerCase())
+      //   ||
+      // article.name.toString()
+      //   .toLowerCase()
+      //   .includes(searchTerm.toLocaleLowerCase())
+      //   ||
+      // article.date
+      //   .toString()
+      //   .toLowerCase()
+      //   .includes(searchTerm.toLocaleLowerCase()) // THIS BREAKS IT ON SOME LETTERS SUCH AS A, C & G, WHY? Because its reading the string "Greenwich Mean Time", so technically all!
+      //   ||
+      // article.Content.toString()
+      //   .toLowerCase()
+      //   .includes(searchTerm.toLocaleLowerCase())// THIS WILL ALLOW THE SEARCH BAR TO SEARCH THROUGH ARTICLE CONTENT, TO MUCH?
+    );
 
-                setSearchResults(fullSearchResults);
-              }, [searchTerm])
-    if(searchTerm) {
-        return (
-          <div>
-            <input
-              type="text"
-              placeholder="Search"
-              value={searchTerm}
-              onChange={handleChange}
-            />
-            {/* <img src={SearchImg} alt=""/> */}
-            <ul className="search-results">
-              {searchResults.map(fullSearchResults => (
-                <Link
-                  to={
-                    fullSearchResults.Interview
-                      ? `/prolist/${fullSearchResults.id}`
-                      : `/postlist/${fullSearchResults.id}`
-                  }
-                >
-                  <li
-                    className="search-results-list-item"
-                    key={fullSearchResults}
-                  >
-                    {fullSearchResults.Title}
-                  </li>
-                </Link>
-              ))}
-            </ul>
-          </div>
-        );
-    } else {
-        return (
-          <div>
-            <input
-              type="text"
-              placeholder="Search"
-              value={searchTerm}
-              onChange={handleChange}
-            />
-          </div>
-        );
-    }
-} 
+    const results2 = Riders.riders.filter(rider =>
+      rider.Title.toString()
+        .toLowerCase()
+        .includes(searchTerm.toLocaleLowerCase())
+    );
+
+    const fullSearchResults = results1.concat(results2);
+    fullSearchResults.sort((a, b) =>
+      a.Title > b.Title ? 1 : b.Title > a.Title ? -1 : 0
+    );
+    console.log(fullSearchResults);
+
+    setSearchResults(fullSearchResults);
+  }, [searchTerm]);
+  if (searchTerm) {
+    return (
+      <div>
+        <input
+          type="text"
+          placeholder="Search"
+          value={searchTerm}
+          onChange={handleChange}
+        />
+        {/* <img src={SearchImg} alt=""/> */}
+        <ul className="search-results">
+          {searchResults.map(fullSearchResults => (
+            <Link
+              to={
+                fullSearchResults.Interview
+                  ? `/prolist/${fullSearchResults.id}`
+                  : `/postlist/${fullSearchResults.id}`
+              }
+            >
+              <li className="search-results-list-item" key={fullSearchResults}>
+                {fullSearchResults.Title}
+              </li>
+            </Link>
+          ))}
+        </ul>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <input
+          type="text"
+          placeholder="Search"
+          value={searchTerm}
+          onChange={handleChange}
+        />
+      </div>
+    );
+  }
+}
 
 export default SearchBar;
