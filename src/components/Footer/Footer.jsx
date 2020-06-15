abf0a2155ae4eff0a8d787f9916f4014ee312d9a
Max Cramer <maximiliancramer@yahoo.com>
2020-06-15 15:24:22 +0100

    finished footer styling and added links to lists for desktop, to style footer for mobile next

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 import IG from '../../Images/Instagram-Icon.png';
 import './Footer.css';
@@ -8,20 +9,28 @@ const Footer = props => (
     <div id="about-quick-links">
       <div id="about-container">
         <h4>About</h4>
-        <p id="about-content">Onewheel News is all about bringing you the latest news from the Onewheel Community, from Pro Rider interviews, the latest & greatest releases from the amazing third party vendors, who's throwing the big tricks and whats going on in the community. We are in no way affiliated with future motion the makers of onewheel, though we think they make one of the best products ever!</p>
+        <p id="about-content">
+          Onewheel News is all about bringing you the latest news from the
+          Onewheel Community, from Pro Rider interviews, the latest & greatest
+          releases from the amazing third party vendors, who's throwing the big
+          tricks and whats going on in the community. We are in no way
+          affiliated with future motion the makers of onewheel, though we think
+          they make one of the best products ever!
+        </p>
       </div>
       <div id="quick-links">
         <h4>Quick Links</h4>
         <ul id="quick-links-list">
-          <li>link</li>
-          <li>link</li>
+          <Link to="/">
+            <li>Home</li>
+          </Link>
+          <Link to="/postlist">
+            <li>Articles</li>
+          </Link>
+          <Link to="/prolist">
+            <li>Interviews</li>
+          </Link>
         </ul>
-      </div>
-      <div id="socials">
-        <h4>Social Links</h4>
-        <a href="https://www.instagram.com/onewheelnews/?hl=en">
-          <img id="ig-logo" src={IG} alt="" />
-        </a>
       </div>
     </div>
     <div id="socials-copyright">
@@ -29,6 +38,12 @@ const Footer = props => (
         Copyright Ⓒ 2020 All Rights Reserved By Onewheel News | No affiliation
         with Future Motion
       </p>
+      <div id="socials">
+        {/* <h4>Social Links</h4> */}
+        <a href="https://www.instagram.com/onewheelnews/?hl=en">
+          <img id="ig-logo" src={IG} alt="" />
+        </a>
+      </div>
     </div>
   </div>
 );
