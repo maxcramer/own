7e0c3602df72816458fffef4e2220c8f6282ec61
Max Cramer <maximiliancramer@yahoo.com>
2020-06-16 11:39:47 +0100

    fixing strange behaviour from yesterday, was getting stuck in else part of if else for input render, now only have one input rendering, value set to '' of input and results is set to an empty array

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,96 +1,67 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Link } from 'react-router-dom';
-
 import SearchImg from '../../Images/search-icon.png';
 import { Articles } from '../../Data/Articles';
 import { Riders } from '../../Data/Riders';
-
 import './SearchBar.css';
-
 function SearchBar() {
   const [searchTerm, setSearchTerm] = React.useState('');
   const [searchResults, setSearchResults] = React.useState([]);
   const handleChange = event => {
     setSearchTerm(event.target.value);
   };
-  
-
   const clearSearch = () => {
-    console.log("running clear search");
-    const input = document.getElementById('input');
-    document.getElementById('search-results').style.display = "none";
-    input.value = '';
-    console.log("the search value is now ", input.value);
-  }
-
+    setSearchTerm('');
+    setSearchResults([]);
+  };
   React.useEffect(() => {
-
-    const results1 = Articles.articles.filter(
-      article =>
+    if (searchTerm !== '') {
+      const results1 = Articles.articles.filter(article =>
         article.Title.toString()
           .toLowerCase()
           .includes(searchTerm.toLocaleLowerCase())
-    );
-
-    const results2 = Riders.riders.filter(rider =>
-      rider.Title.toString()
-        .toLowerCase()
-        .includes(searchTerm.toLocaleLowerCase())
-    );
-
-    const fullSearchResults = results1.concat(results2);
-    fullSearchResults.sort((a, b) =>
-      a.Title > b.Title ? 1 : b.Title > a.Title ? -1 : 0
-    );
-    console.log(fullSearchResults);
-
-    
-    setSearchResults(fullSearchResults);
-
+      );
+      const results2 = Riders.riders.filter(rider =>
+        rider.Title.toString()
+          .toLowerCase()
+          .includes(searchTerm.toLocaleLowerCase())
+      );
+      const fullSearchResults = results1.concat(results2);
+      fullSearchResults.sort((a, b) =>
+        a.Title > b.Title ? 1 : b.Title > a.Title ? -1 : 0
+      );
+      console.log(fullSearchResults);
+      setSearchResults(fullSearchResults);
+    }
   }, [searchTerm]);
-
-  if (searchTerm) {
-    return (
-      <div>
-        <input
-          type="text"
-          placeholder="Search"
-          value={searchTerm}
-          id="input"
-          onChange={handleChange}
-        />
-        {/* <img src={SearchImg} alt=""/> */}
-        <ul id="search-results" className="search-results">
-          {searchResults.map(fullSearchResults => (
-            <li className="search-results-list-item" key={fullSearchResults.id}>
-              <Link
-                onClick={clearSearch}
-                to={
-                  fullSearchResults.Interview
-                    ? `/prolist/${fullSearchResults.id}`
-                    : `/postlist/${fullSearchResults.id}`
-                }
-              >
-                {fullSearchResults.Title}
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </div>
-    );
-  } else {
-    return (
-      <div>
-        <input
-          type="text"
-          placeholder="Search"
-          value={searchTerm}
-          onChange={handleChange}
-        />
-      </div>
-    );
-  }
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Search"
+        value={searchTerm}
+        id="input"
+        onChange={handleChange}
+      />
+      {/* <img src={SearchImg} alt=""/> */}
+      <ul id="search-results-full-list" className="search-results">
+        {searchResults.map(fullSearchResults => (
+          <li className="search-results-list-item" key={fullSearchResults.id}>
+            <Link
+              onClick={clearSearch}
+              to={
+                fullSearchResults.Interview
+                  ? `/prolist/${fullSearchResults.id}`
+                  : `/postlist/${fullSearchResults.id}`
+              }
+            >
+              {fullSearchResults.Title}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
-
 export default SearchBar;
