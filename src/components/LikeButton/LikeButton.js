661e117cfb301980cd3ef23d3d1064b9e56d81c5
maxcramer <maximiliancramer@yahoo.com>
2020-07-09 15:41:15 +0100

    am able to console log articles number of likes but not able to get it in render

---
+++ src/components/LikeButton/LikeButton.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from 'react';
+import { Link, useParams } from 'react-router-dom';
+import { getPostList } from '../../services/firestoreService';
+
+import firebaseClient from '../../firebaseClient';
+
+
+function ArticlesLikeButton () {
+  const [articles, setArticles] = useState();
+  const [article, setArticle] = useState();
+  let { id } = useParams();
+  const [likes, setLikes] = useState();
+  const [like, setLike] = useState();
+
+  useEffect(() => {
+    async function fetchData() {
+      const articles = await getPostList();
+      setArticles(articles);
+      const match = articles.find(article => article.id === parseInt(id));
+      console.log('trying to console log article likes', match.likes) 
+      setArticle(match);  
+    }
+    fetchData();
+  }, [id])
+
+  return (
+    
+    <div>
+      <p>{id.likes}</p>
+      <button>
+        this is a button
+      </button>
+    </div>
+  )
+}
+
+export default ArticlesLikeButton;
--- src/components/ProRiders/pros/Rider.js
+++ src/components/ProRiders/pros/Rider.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useParams } from 'react-router-dom'
 import { getRiderList } from '../../../services/firestoreService';
+
 import firebaseClient from '../../../firebaseClient';
 
 import '../proRiders.css';
