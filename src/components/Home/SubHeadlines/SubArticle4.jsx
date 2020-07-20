5f01af3ccecdf1b6adab4028a14e1198a6828e70
maxcramer <maximiliancramer@yahoo.com>
2020-07-20 15:37:39 +0100

    SubArticle4 rendering data from firebase database

--- src/components/Home/SubHeadlines/SubArticle4.jsx
+++ src/components/Home/SubHeadlines/SubArticle4.jsx
@@ -1,30 +1,41 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Link, useParams } from 'react-router-dom';
+import { getPostList } from '../../../services/firestoreService';
 
-import { Articles } from '../../../Data/Articles';
 import './SubArticles.css';
 
-class SubArticleFour extends React.Component {
-    render() {
-        const subArticleFour = Articles.articles.sort((a, b) => b.date - a.date)[4]
-        if(subArticleFour) {
+function SubArticleFour () {
+    const [articles, setArticles] = useState();
+    const [article, setArticle] = useState();
+    let { id } = useParams();
+
+
+    useEffect(() => {
+        async function fetchData() {
+            const articles = await getPostList();
+            setArticles(articles);
+            const match = articles[4];
+            setArticle(match);
+        }
+        fetchData();
+    }, [id])
+        if(article) {
             return (
                 <div className="single_Sub_Article">
-                    <h2>{subArticleFour.Title}</h2>
-                    <img className="Sub_Article_Img" src={subArticleFour.image} alt="" />
+                    <h2>{article.Title}</h2>
+                    <img className="Sub_Article_Img" src={article.image} alt="" />
                     <div className="Sub_Article_Auth_Date">
-                        <h4 className="Sub_Article_name">By {subArticleFour.name}</h4>
-                        <p className="Sub_Article_Date">Uploaded {subArticleFour.date.toLocaleString()}</p>
+                        <h4 className="Sub_Article_name">By {article.Author}</h4>
+                        <p className="Sub_Article_Date">Uploaded {new Date(article.date).toDateString()}</p>
                     </div>
-                    <p className="Sub_Article_Content">{subArticleFour.Content}</p>
-                    <Link className="read_more_tag" to={`/postlist/${subArticleFour.id}`}>Read More</Link>
+                    <p className="Sub_Article_Content">{article.Content}</p>
+                    <Link className="read_more_tag" to={`/postlist/${article.id}`}>Read More</Link>
                 </div>
             )
 
         } else {
-            return null;
+            return <div>Article 4 loading</div>;
         }
-    }
 };
 
 export default SubArticleFour;
