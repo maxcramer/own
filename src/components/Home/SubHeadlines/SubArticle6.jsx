e2c267d4562562f7e476eaa209ca7355b596fe0c
maxcramer <maximiliancramer@yahoo.com>
2020-07-20 15:50:32 +0100

    hooking up SubArticle 6 to firebase database

--- src/components/Home/SubHeadlines/SubArticle6.jsx
+++ src/components/Home/SubHeadlines/SubArticle6.jsx
@@ -2,21 +2,32 @@ import React, { useState, useEffect } from 'react';
 import {Link, useParams} from 'react-router-dom';
 import { getPostList } from '../../../services/firestoreService';
 
-
 import './SubArticles.css';
 
 function SubArticleSix () {
-        if (subArticleSix) {
+    const [articles, setArticles] = useState();
+    const [article, setArticle] = useState();
+    let { id } = useParams();
+    useEffect(() => {
+        async function fetchData() {
+            const articles = await getPostList();
+            setArticles(articles);
+            const match = articles[6];
+            setArticle(match);
+        }
+        fetchData();
+    }, [id])
+        if (article) {
             return (
                 <div className="single_Sub_Article">
-                    <h2>{subArticleSix.Title}</h2>
-                    <img className="Sub_Article_Img" src={subArticleSix.image} alt="" />
+                    <h2>{article.Title}</h2>
+                    <img className="Sub_Article_Img" src={article.image} alt="" />
                     <div className="Sub_Article_Auth_Date">
-                        <h4 className="Sub_Article_name">By {subArticleSix.name}</h4>
-                        <p className="Sub_Article_Date">Uploaded {subArticleSix.date.toLocaleString()}</p>
+                        <h4 className="Sub_Article_name">By {article.name}</h4>
+                        <p className="Sub_Article_Date">Uploaded {new Date(article.date).toDateString()}</p>
                     </div>
-                    <p className="Sub_Article_Content">{subArticleSix.Content}</p>
-                    <Link className="read_more_tag" to={`/postlist/${subArticleSix.id}`}>Read More</Link>
+                    <p className="Sub_Article_Content">{article.Content}</p>
+                    <Link className="read_more_tag" to={`/postlist/${article.id}`}>Read More</Link>
                 </div>
             )
 
