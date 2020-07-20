5cc863e1217bb54e8185471709b05d7a40cce64f
maxcramer <maximiliancramer@yahoo.com>
2020-07-20 13:08:10 +0100

    rewritting headline article to take data from firebase instead of locally

--- src/components/Home/HeadlineArticle/HeadlineArticle.jsx
+++ src/components/Home/HeadlineArticle/HeadlineArticle.jsx
@@ -1,26 +1,41 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import {Link, useParams} from 'react-router-dom';
+import { getPostList } from '../../../services/firestoreService'
 
-import {Articles} from '../../../Data/Articles';
+// import {Articles} from '../../../Data/Articles';
 import './HeadlineArticle.css';
 
-class HeadlineArticle extends React.Component {
-    render() {
-        const recentArticle = Articles.articles.sort((a, b) => b.date - a.date)[0]
-        if(recentArticle) {
+function HeadlineArticle () {
+    const [articles, setArticles] = useState();
+    const [article, setArticle] = useState();
+    let { id } = useParams();
+
+    useEffect(() => {
+        async function fetchData() {
+            const articles = await getPostList();
+            setArticles(articles);
+            const match = articles.find(article => article.id === parseInt(id)[0]);
+            console.log('match top article', match);
+            setArticle(match);
+        }
+        fetchData();
+    }, [id])
+
+        // const firstArticle = article.sort((a, b) => b.date - a.date)[0]
+        if(article) {
             return (
                 <div className="headline_container">
                     <div>
-                        <img className="headline_img" src={recentArticle.image} alt="article image"/>
+                        <img className="headline_img" src={article.image} alt="article image"/>
                     </div>
                     <div className="headline_text">
-                        <h2 className="headline_title">{recentArticle.Title}</h2>
+                        <h2 className="headline_title">{article.Title}</h2>
                         <div className="headline_info">
-                            <h4 className="mobile_name">By {recentArticle.name}</h4>
-                            <p className="mobile_date">Uploaded on {recentArticle.date.toLocaleString()}</p>
+                            <h4 className="mobile_name">By {article.name}</h4>
+                            <p className="mobile_date">Uploaded on {article.date.toLocaleString()}</p>
                         </div>
-                        <p className="headline_article">{recentArticle.Content}</p>
-                        <Link className="read_more_tag" to={`/postlist/${recentArticle.id}`}>Read More</Link>
+                        <p className="headline_article">{article.Content}</p>
+                        <Link className="read_more_tag" to={`/postlist/${article.id}`}>Read More</Link>
     
                     </div>
                 </div>
@@ -29,7 +44,6 @@ class HeadlineArticle extends React.Component {
         } else {
             return null;
         }
-    }
-};
+    };
 
 export default HeadlineArticle;
