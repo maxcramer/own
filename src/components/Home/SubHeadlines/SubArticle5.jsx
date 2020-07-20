640a7bc083f8a8dfd596130906fd024a0bcf0ceb
maxcramer <maximiliancramer@yahoo.com>
2020-07-20 15:45:29 +0100

    hooking up SubArticle 5 to firebase database

--- src/components/Home/SubHeadlines/SubArticle5.jsx
+++ src/components/Home/SubHeadlines/SubArticle5.jsx
@@ -1,22 +1,35 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { Link, useParams } from 'react-router-dom';
+import { getPostList } from '../../../services/firestoreService';
 
 import './SubArticles.css';
 
 function SubArticleFive () {
+    const [articles, setArticles] = useState();
+    const [article, setArticle] = useState();
+    let { id } = useParams();
 
-    
-            if(subArticleFive) {
+    useEffect(() => {
+        async function fetchData() {
+            const articles = await getPostList();
+            setArticles(articles);
+            const match = articles[5];
+            setArticle(match);
+        }
+        fetchData();
+    }, [id])
+
+            if(article) {
             return (
                     <div className="single_Sub_Article">
-                        <h2>{subArticleFive.Title}</h2>
-                        <img className="Sub_Article_Img" src={subArticleFive.image} alt="" />
+                        <h2>{article.Title}</h2>
+                        <img className="Sub_Article_Img" src={article.image} alt="" />
                         <div className="Sub_Article_Auth_Date">
-                            <h4 className="Sub_Article_name">By {subArticleFive.name}</h4>
-                            <p className="Sub_Article_Date">Uploaded {subArticleFive.date.toLocaleString()}</p>
+                            <h4 className="Sub_Article_name">By {article.Author}</h4>
+                            <p className="Sub_Article_Date">Uploaded {new Date(article.date).toDateString()}</p>
                         </div>
-                        <p className="Sub_Article_Content">{subArticleFive.Content}</p>
-                        <Link className="read_more_tag" to={`/postlist/${subArticleFive.id}`}>Read More</Link>
+                        <p className="Sub_Article_Content">{article.Content}</p>
+                        <Link className="read_more_tag" to={`/postlist/${article.id}`}>Read More</Link>
                     </div>
                 )
         } else {
