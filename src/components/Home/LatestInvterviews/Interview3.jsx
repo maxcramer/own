e2759006968eb74e92ab6fb80912133b0763fcd0
maxcramer <maximiliancramer@yahoo.com>
2020-08-03 14:54:47 +0100

    interview 3 on home page now loading from firebase database, damn this text styling on the interviews is the ugliest thing ive ever seen! dear god what was i thinking

--- src/components/Home/LatestInvterviews/Interview3.jsx
+++ src/components/Home/LatestInvterviews/Interview3.jsx
@@ -18,23 +18,29 @@ function InterviewThree () {
         }
         fetchData();
     }, [id])
-    return (
+    if(riderInterview) {
+        return (
             <div className="pro_rider_interview">
-                <img className="rider_image" src={recentInterviewThree.Logo} alt="" />
+                <img className="rider_image" src={riderInterview.Logo} alt="" />
                 <div className="rider_info">
                     <div className="rider_name_location">
-                        <h2 className="rider_name">{recentInterviewThree.Title}</h2>
-                        <h6 className="upload_date">Date Uploaded: {recentInterviewThree.dateUpload.toLocaleString()}</h6>
+                        <h2 className="rider_name">{riderInterview.Title}</h2>
+                        <h6 className="upload_date">Date Uploaded: {new Date(riderInterview.date).toDateString()}</h6>
                     </div>
                     <div className="sponsor_location">
-                        <h4 className="sponsors">Sponsors: {recentInterviewThree.Sponsonrs}</h4>
-                        <h4 className="location">Location: {recentInterviewThree.location}</h4>
+                        <h4 className="sponsors">Sponsors: {riderInterview.Sponsonrs}</h4>
+                        <h4 className="location">Location: {riderInterview.location}</h4>
                     </div>
-                    <p className="interview">{recentInterviewThree.Interview}</p>
-                    <Link className="read_interview" to={`/prolist/${recentInterviewThree.id}`}>Read Interview</Link>
+                    <p className="interview">{riderInterview.Interview}</p>
+                    <Link className="read_interview" to={`/prolist/${riderInterview.id}`}>Read Interview</Link>
                 </div>
             </div>
         )
+    } else {
+        return <div>Latest rider interview loading</div>
+    }
+
+
 };
 
 export default InterviewThree;
