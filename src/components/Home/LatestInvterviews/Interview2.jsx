4e64cfa6d5696492e505a02e666b6001d15eef7a
maxcramer <maximiliancramer@yahoo.com>
2020-08-03 14:44:31 +0100

    interview 2 on home page changed from class based component to fuctional component, seriously, what the hell was I thinking when i made the text area on this page for the interviews so smallgit add .! OMG

--- src/components/Home/LatestInvterviews/Interview2.jsx
+++ src/components/Home/LatestInvterviews/Interview2.jsx
@@ -1,42 +1,54 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import {Link, useParams} from 'react-router-dom';
+import { getRiderList } from '../../../services/firestoreService';
 
-import { Riders } from '../../../Data/Riders';
 import IG from '../../../Images/Instagram-Icon.png'
 
-class InterviewTwo extends React.Component {
-    render() {
-        const recentInterviewTwo = Riders.riders.sort((a, b) => b.dateUpload - a.dateUpload)[1]
-        return (
-          <div className="pro_rider_interview">
-            <img className="rider_image" src={recentInterviewTwo.Logo} alt="" />
-            <div className="rider_info">
-              <div className="rider_name_location">
-                <h2 className="rider_name">{recentInterviewTwo.Title}</h2>
-                <h6 className="upload_date">
-                  Date Uploaded:{' '}
-                  {recentInterviewTwo.dateUpload.toLocaleString()}
-                </h6>
-              </div>
-              <div className="sponsor_location">
-                <h4 className="sponsors">
-                  Sponsors: {recentInterviewTwo.Sponsonrs}
-                </h4>
-                <h4 className="location">
-                  Location: {recentInterviewTwo.location}
-                </h4>
-              </div>
-              <p className="interview">{recentInterviewTwo.Interview}</p>
-              <Link
-                className="read_interview"
-                to={`/prolist/${recentInterviewTwo.id}`}
-              >
-                Read Interview
-              </Link>
-            </div>
-          </div>
-        );
+function InterviewTwo () {
+  const [riderInterviews, setRiderInterviews] = useState();
+  const [riderInterview, setRiderInterview] = useState();
+  let { id } = useParams();
+
+  useEffect(() => {
+    async function fetchData() {
+      const riderInterviews = await getRiderList();
+      setRiderInterviews(riderInterviews);
+      const match = riderInterviews[1];
+      setRiderInterview(match);
     }
+    fetchData();
+  }, [id])
+
+  if(riderInterview) {
+    return (
+      <div className="pro_rider_interview">
+        <img className="rider_image" src={riderInterview.Logo} alt="" />
+        <div className="rider_info">
+          <div className="rider_name_location">
+            <h2 className="rider_name">{riderInterview.Title}</h2>
+            <h6 className="upload_date">Date Uploaded: {new Date(riderInterview.date).toDateString()}</h6>
+          </div>
+          <div className="sponsor_location">
+            <h4 className="sponsors">
+              Sponsors: {riderInterview.Sponsonrs}
+            </h4>
+            <h4 className="location">
+              Location: {riderInterview.location}
+            </h4>
+          </div>
+          <p className="interview">{riderInterview.Interview}</p>
+          <Link
+            className="read_interview"
+            to={`/prolist/${riderInterview.id}`}
+          >
+            Read Interview
+          </Link>
+        </div>
+      </div>
+    );
+  } else {
+    return <div>Latest rider interview loading</div>
+}
 };
 
 export default InterviewTwo;
