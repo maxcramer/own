f722c678f39b208f0aa6f3bae7468496bfda3ff7
maxcramer <maximiliancramer@yahoo.com>
2020-09-16 13:10:50 +0100

    pair coding with michael to clear input on search bar

--- web-app/src/components/SearchBar/SearchBar.jsx
+++ web-app/src/components/SearchBar/SearchBar.jsx
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from 'react';
-import ReactDOM from 'react-dom';
+import React, { useState, createRef, useEffect } from 'react';
+// import ReactDOM from 'react-dom';
 import { Link, useParams, Router } from 'react-router-dom';
-import SearchImg from '../../Images/search-icon.png';
+// import SearchImg from '../../Images/search-icon.png';
 import { getRiderList, getPostList } from '../../services/firestoreService'
 import './SearchBar.css';
 
@@ -11,26 +11,11 @@ function SearchBar() {
   const [riders, setRiders] = useState([]);
   const [posts, setPosts] = useState([]);
   const [results, setResults] = useState([]);
+  const searchInput = createRef();
   
-  // THIS DOESNT WORK
-  // const clearSearch = () => {
-  //   let inputElement = document.getElementById('input');
-  //   inputElement.value = '';
-  //   console.log("search being cleared:", setResults);
-  //   setResults([]) 
-    // ^^^ this needs to be empty or all 
-    // posts and interviews are visible in search results 
-    // on click because we are using concat on all results. was using method ... to join
-//  };
 
-// onResultClick {
-//   ...
-//   Router.go(link)
-//   }
-
-
- const filterResults = event => {
-  const searchTerm = event.target.value;
+ const filterResults = () => {
+  const searchTerm = searchInput.current.value;
   const postResults = posts.filter(post => post.Title.toLowerCase().includes(searchTerm.toLowerCase()));
   const riderResults = riders.filter(rider => rider.Name.toLowerCase().includes(searchTerm.toLowerCase()));
   setResults([...postResults, ...riderResults]);
@@ -38,14 +23,13 @@ function SearchBar() {
 }
 
 const onResultClick = () => {
-  console.log("this runs alot");
-  // setRiders([])
-  // setPosts([])
-  setResults([])
+  console.log("this runs a lot");
+  setResults([]);  
+  searchInput.current.value = '';
 }
 
 
-  React.useEffect(() => {
+useEffect(() => {
     
     const fetchRiders = async () => {
       const ridersResponse = await getRiderList();
@@ -70,7 +54,7 @@ const onResultClick = () => {
       <input
         type="text"
         placeholder="Search"
-        id="input"
+        ref={searchInput}
         onChange={filterResults}
       />
       <div id="search-results-container-div">
